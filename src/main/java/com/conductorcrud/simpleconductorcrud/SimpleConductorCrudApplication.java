fb54cabb0dd642eeb57bce3468ebae47a3384dd1
Edson Alves <edson.filho@conductor.com.br>
2020-01-08 13:40:16 -0200

    Initial commit

---
+++ src/main/java/com/conductorcrud/simpleconductorcrud/SimpleConductorCrudApplication.java
@@ -0,0 +1,13 @@
+package com.conductorcrud.simpleconductorcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleConductorCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleConductorCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conductorcrud/simpleconductorcrud/SimpleConductorCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conductorcrud.simpleconductorcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleConductorCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
